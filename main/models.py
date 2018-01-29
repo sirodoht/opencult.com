4804bbf35bb3525392fb23108e7376a40118e975
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-30 01:21:33 +0200

    sort models imports

--- main/models.py
+++ main/models.py
@@ -1,8 +1,8 @@
-from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 
 class Profile(models.Model):

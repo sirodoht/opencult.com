c9e32be70a129dc19ababc523d672082141174cb
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-29 23:37:53 +0200

    add urls for auth and login and logout

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
+
+admin.site.site_header = 'Open Cult administration'
+app_name = 'main'
+
 urlpatterns = [
+    # /
     path('', views.index, name='index'),
+
+    # /login/
+    path('login/', views.get_login, name='login'),
+
+    # /auth/
+    path('auth/', views.token_post, name='auth'),
+
+    # /logout/
+    path('logout/', views.get_logout, name='logout'),
 ]

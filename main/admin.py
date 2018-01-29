bfefa8cdace85cdf6f472f2fc4dd2ec041541694
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-29 23:37:21 +0200

    add profile on backend

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile
+
+
+# Users
+class OpCuAdmin(UserAdmin):
+    list_display = ('id', 'username', 'date_joined', 'last_login',)
+
+admin.site.unregister(User)
+admin.site.register(User, OpCuAdmin)

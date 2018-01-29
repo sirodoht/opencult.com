1dbbcf4bf6e309dd73fc8e3a470fb60fe821e67b
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-30 01:20:59 +0200

    add cults, events, membership and attendance models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
@@ -6,7 +7,10 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    about = models.TextField(max_length=500, blank=True)
+    about = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
@@ -18,3 +22,54 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Cult(models.Model):
+    members = models.ManyToManyField(User, through='Membership')
+    name = models.CharField(max_length=100)
+    doctrine = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    cult = models.ForeignKey(Cult, on_delete=models.CASCADE)
+    attendees = models.ManyToManyField(User, through='Attendance')
+    title = models.CharField(max_length=100)
+    details = models.TextField(blank=True, null=True)
+    when = models.DateTimeField(default=timezone.now)
+    venue = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Membership(models.Model):
+    cult = models.ForeignKey(Cult, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    ADMIN = 'admin'
+    MEMBER = 'member'
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (MEMBER, 'Member'),
+    )
+    role = models.CharField(
+        choices=ROLE_CHOICES,
+        max_length=50,
+    )
+
+    def __str__(self):
+        return self.cult.name + ' :: ' + self.user.username
+
+
+class Attendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_rsvped = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.event.user + ' :: ' + self.user.username

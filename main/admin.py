e9f0e1b85255ff0f84b05150694c54cd7f0bf57d
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-30 01:29:26 +0200

    add models on admin

--- main/admin.py
+++ main/admin.py
@@ -2,12 +2,44 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Attendance, Cult, Event, Membership, Profile
 
 
-# Users
+# User
 class OpCuAdmin(UserAdmin):
     list_display = ('id', 'username', 'date_joined', 'last_login',)
 
 admin.site.unregister(User)
 admin.site.register(User, OpCuAdmin)
+
+
+# Profile
+admin.site.register(Profile)
+
+
+# Cult
+class CultAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location',)
+
+admin.site.register(Cult, CultAdmin)
+
+
+# Event
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cult', 'when', 'venue',)
+
+admin.site.register(Event, EventAdmin)
+
+
+# Membership
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('cult', 'user', 'role', 'date_joined',)
+
+admin.site.register(Membership, MembershipAdmin)
+
+
+# Attendance
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('event', 'user', 'date_rsvped',)
+
+admin.site.register(Attendance, AttendanceAdmin)

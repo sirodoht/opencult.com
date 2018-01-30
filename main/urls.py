01bd082c25eaf91059283498c4d4088d6ccc5c38
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-30 05:06:38 +0200

    add cult and event urls

--- main/urls.py
+++ main/urls.py
@@ -18,4 +18,10 @@ urlpatterns = [
 
     # /logout/
     path('logout/', views.get_logout, name='logout'),
+
+    # /cult/
+    path('cult/', views.get_cult, name='cult'),
+
+    # /event/
+    path('event/', views.get_event, name='event'),
 ]

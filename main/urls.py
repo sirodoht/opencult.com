3317255e0c336edb2caec194f0ff3f4c7af41903
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-31 23:55:22 +0200

    add edit event url

--- main/urls.py
+++ main/urls.py
@@ -34,6 +34,9 @@ urlpatterns = [
     # e.g. /some-cult-slug/edit/
     path('<slug:cult_slug>/edit/', views.edit_cult, name='edit_cult'),
 
+    # e.g. /some-cult-slug/some-event-slug/edit
+    path('<slug:cult_slug>/<slug:event_slug>/edit/', views.edit_event, name='edit_event'),
+
     # e.g. /some-cult-slug/some-event-slug/
     path('<slug:cult_slug>/<slug:event_slug>/', views.event, name='event'),
 

4a09537e821712af69643283fc1996b176fc9081
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-31 02:16:40 +0200

    add new cult form

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from .models import Cult
+
 
 class EmailForm(forms.Form):
     email = forms.EmailField(label='Your email address')
+
+
+class CultForm(forms.ModelForm):
+    class Meta:
+        model = Cult
+        fields = ['name', 'doctrine', 'city', 'country']

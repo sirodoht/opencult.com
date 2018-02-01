163ea8235299cbe4a8570a59aaa8ec8d614b5792
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-01 18:06:56 +0200

    add generate username helper method

---
+++ main/helpers.py
@@ -0,0 +1,12 @@
+import shortuuid
+from django.contrib.auth.models import User
+
+def generate_username(email):
+    username = email.split('@')[0]
+
+    # check if exists
+    if User.objects.filter(username=username).count():
+        uuid = shortuuid.ShortUUID('abdcefghkmnpqrstuvwxyzABDCEFGHKMNPQRSTUVWXYZ23456789').random(length=12)
+        username += uuid
+
+    return username

a019b61ecb730af18851321206194fe8dabd1848
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-01 19:33:16 +0200

    add edit user tests

--- main/tests/test_user.py
+++ main/tests/test_user.py
@@ -1,7 +1,7 @@
 import pytest
 from django.test import Client
 
-from main.forms import EmailForm
+from main.forms import EmailForm, UserForm
 
 
 def test_get_login():
@@ -10,9 +10,15 @@ def test_get_login():
     assert response.status_code == 200
 
 
-def test_email_form(django_user_model):
+def test_email_form():
     c = Client()
     response = c.post('/login/')
+    assert response.status_code == 200
+
+
+def test_get_login():
+    c = Client()
+    response = c.get('/login/')
     assert response.status_code == 200
 
 
@@ -32,3 +38,43 @@ def test_email_form(django_user_model):
     response = c.get('/login/')
     assert response.status_code == 200
     assert b'Login email sent! Please check your inbox and click on the link.' in response.content
+
+
+@pytest.mark.django_db()
+def test_logout(django_user_model):
+    user = django_user_model.objects.create(username='mother')
+    user.set_password('takeajacket')
+    user.save()
+    form_data = {
+        'email': 'daddy@cool.com',
+    }
+    form = EmailForm(data=form_data)
+    assert form.is_valid()
+    c = Client()
+    logged_in = c.login(username='mother', password='takeajacket')
+    assert logged_in
+    response = c.get('/logout/', follow=True)
+    assert response.status_code == 200
+    assert b'You have been logged out.' in response.content
+
+
+@pytest.mark.django_db()
+def test_edit_user(django_user_model):
+    user = django_user_model.objects.create(username='mother')
+    user.set_password('takeajacket')
+    user.profile.about = 'Built a wall.'
+    user.save()
+    form_data = {
+        'username': 'daddycool',
+        'about': 'Watching Grease',
+    }
+    form = UserForm(data=form_data)
+    assert form.is_valid()
+    c = Client()
+    logged_in = c.login(username='mother', password='takeajacket')
+    assert logged_in
+    response = c.post('/@' + user.username + '/edit/', form_data, follow=True)
+    assert response.status_code == 200
+    assert b'Profile updated' in response.content
+    assert form_data['username'].encode() in response.content
+    assert form_data['about'].encode() in response.content

20d6de0d1417a112fa5e634ffbf2012337f9c2bb
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-01 20:36:31 +0200

    move profile test to user test

--- main/tests/test_user.py
+++ main/tests/test_user.py
@@ -2,18 +2,43 @@ import pytest
 from django.test import Client
 
 from main.forms import EmailForm, UserForm
+from main.models import Cult, Membership, Profile
 
 
-def test_get_login():
+@pytest.mark.django_db()
+def test_profile(django_user_model):
+    user = django_user_model.objects.create(username='mother', email='setme@free.com')
+    profile = Profile.objects.get(user=user)
+    profile.about = 'Under the moonlight.'
+    profile.save()
+    cult_1 = Cult.objects.create(
+        name='Sweet Dreams',
+        slug='sweet-dreams',
+        doctrine='Hold your head up, keep your head up!',
+        city='Amsterdam',
+    )
+    cult_2 = Cult.objects.create(
+        name='The night is on fire',
+        slug='night-on-fire',
+        doctrine='What you say!',
+        city='Barcelona',
+    )
+    Membership.objects.create(
+        cult=cult_1,
+        user=user,
+        role=Membership.LEADER,
+    )
+    Membership.objects.create(
+        cult=cult_2,
+        user=user,
+        role=Membership.MEMBER,
+    )
     c = Client()
-    response = c.get('/login/')
+    response = c.get('/@' + user.username + '/')
     assert response.status_code == 200
-
-
-def test_email_form():
-    c = Client()
-    response = c.post('/login/')
-    assert response.status_code == 200
+    assert profile.about.encode() in response.content
+    assert cult_1.name.encode() in response.content
+    assert cult_2.name.encode() in response.content
 
 
 def test_get_login():

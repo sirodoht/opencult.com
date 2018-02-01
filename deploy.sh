ec41235958edb3fc39a118e88b71c92866d791c6
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-01 15:54:00 +0200

    comment out keyscan line from deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 if [ $BRANCH == "master" ]; then
     echo "Deployment starting..."
-    ssh-keyscan -H opencult.com >> ~/.ssh/known_hosts
+    # ssh-keyscan -H opencult.com >> ~/.ssh/known_hosts
     git push dokku@opencult.com:opencult master
 else
     echo "Not on master, deployment canceled."

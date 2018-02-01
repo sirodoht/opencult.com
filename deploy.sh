e0fea3ae9e1b6651215a30eca62e05cf2ce6ba44
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-01 15:40:25 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ $BRANCH == "master" ]; then
+    echo "Deployment starting..."
+    ssh-keyscan -H opencult.com >> ~/.ssh/known_hosts
+    git push dokku@opencult.com:opencult master
+else
+    echo "Not on master, deployment canceled."
+fi

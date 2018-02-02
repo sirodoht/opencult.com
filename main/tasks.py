1d0c593521d92cb38cf633405666d70c8ed64e3a
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-02 22:14:23 +0200

    add tasks module on main app

---
+++ main/tasks.py
@@ -0,0 +1,21 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+
+from opencult import settings
+
+
+@shared_task
+def announce_event(data):
+    send_mail(
+        data['cult_name'] + ' announcement: ' + data['event_title'] + ' event',
+        render_to_string(
+            'main/announce_event_email.txt',
+            {'data': data},
+        ),
+        settings.DEFAULT_FROM_EMAIL,
+        [data['member_email']],
+    )
+
+    return 'done'

52d9bf5850cc3f402626f6cd2ce1e11c9f5c2497
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-02 22:16:25 +0200

    remove unused current site import from tasks

--- main/tasks.py
+++ main/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 
 from opencult import settings

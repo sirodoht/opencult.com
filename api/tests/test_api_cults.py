6689456215be8ed0ebe64e951ee9ab8dbc765aab
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-05 03:52:47 +0200

    add api cult tests

---
+++ api/tests/test_api_cults.py
@@ -0,0 +1,48 @@
+import pytest
+from django.test import Client
+
+from main.models import Cult
+
+
+@pytest.mark.django_db()
+def test_api_cults():
+    cult_1 = Cult.objects.create(
+        name='Sweet Dreams',
+        slug='sweet-dreams',
+        doctrine='Hold your head up, keep your head up!',
+        city='Amsterdam',
+    )
+    cult_2 = Cult.objects.create(
+        name='Illumination Theory',
+        slug='illumination-theory',
+        doctrine='Consider this question, open your eyes.',
+        city='Boston',
+    )
+    c = Client()
+    response = c.get('/api/cults/')
+    assert response.status_code == 200
+    assert cult_1.name.encode() in response.content
+    assert cult_1.slug.encode() in response.content
+    assert cult_1.doctrine.encode() in response.content
+    assert cult_1.city.encode() in response.content
+    assert cult_2.name.encode() in response.content
+    assert cult_2.slug.encode() in response.content
+    assert cult_2.doctrine.encode() in response.content
+    assert cult_2.city.encode() in response.content
+
+
+@pytest.mark.django_db()
+def test_api_cult_single():
+    cult = Cult.objects.create(
+        name='Sweet Dreams',
+        slug='sweet-dreams',
+        doctrine='Hold your head up, keep your head up!',
+        city='Amsterdam',
+    )
+    c = Client()
+    response = c.get('/api/cults/' + cult.slug + '/')
+    assert response.status_code == 200
+    assert cult.name.encode() in response.content
+    assert cult.slug.encode() in response.content
+    assert cult.doctrine.encode() in response.content
+    assert cult.city.encode() in response.content

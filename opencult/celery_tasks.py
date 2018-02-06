17f1546fc924c44df337ad2dcbdaaefd18200788
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-02-06 13:13:13 +0200

    use dotenv on celery

--- opencult/celery_tasks.py
+++ opencult/celery_tasks.py
@@ -1,6 +1,11 @@
 import os
+from os.path import dirname, join
 
 from celery import Celery
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opencult.settings')
 

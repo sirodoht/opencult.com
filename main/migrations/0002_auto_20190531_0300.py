b19b7a65f5fff33cb80564bca50d4cde662c01e1
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-05-31 06:23:51 +0300

    change migration formatting to black

--- main/migrations/0002_auto_20190531_0300.py
+++ main/migrations/0002_auto_20190531_0300.py
@@ -5,14 +5,16 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('main', '0001_initial'),
-    ]
+    dependencies = [("main", "0001_initial")]
 
     operations = [
         migrations.AlterField(
-            model_name='membership',
-            name='role',
-            field=models.CharField(choices=[('organizer', 'Organizer'), ('member', 'Member')], default='member', max_length=50),
-        ),
+            model_name="membership",
+            name="role",
+            field=models.CharField(
+                choices=[("organizer", "Organizer"), ("member", "Member")],
+                default="member",
+                max_length=50,
+            ),
+        )
     ]
